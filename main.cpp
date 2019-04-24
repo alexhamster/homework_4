dbe04ef647dbf87399c550bb151232cd25a3ff43
Alex <soliaris2012@mail.ru>
2019-04-24 20:58:47 +0300

    tuple check size fix

--- main.cpp
+++ main.cpp
@@ -20,9 +20,11 @@ namespace myFunc {
         return IsTupleImpl<std::decay_t<T>>::value;
     }
 
-
     template <typename T, typename U, size_t S, typename V>
     struct is_one_of : std::false_type {};
+
+    template <typename T, typename V>
+    struct is_one_of<T,T,1,V> : std::true_type {};
 
     template <typename T, typename V>
     struct is_one_of<T,T,0,V> : std::true_type {};
@@ -30,56 +32,82 @@ namespace myFunc {
     template <typename T, size_t S, typename V>
     struct is_one_of<T,T,S,V> : is_one_of<typename std::tuple_element<0,V>::type,
             typename std::tuple_element<S,V>::type, S-1, V> {};
+
+
 }
 
-template<typename T, size_t S, bool is_integral, bool is_object, bool is_tuple, bool is_valid_tuple>
+template<typename T, size_t S, bool is_integral, bool is_object, bool is_tuple>
 struct print // default case
 {
     const static int method = 0;
+    void operator()(T t)
+    {
+        std::cout << "Printing NOTHING" << std::endl;
+    }
 };
 
 template<typename T, size_t S>
-struct print<T, S, true, false, false, false> // integral types
+struct print<T, S, true, false, false> // integral types
 {
     const static int method = 1;
+    void operator()(T t)
+    {
+        std::cout << "Printing integral" << std::endl;
+    }
 };
 
 template<typename T, size_t S>
-struct print<T, S, false, true, false, false> // object types
+struct print<T, S, false, true, false> // object types
 {
     const static int method = 2;
+    void operator()(T t)
+    {
+        std::cout << "Printing object" << std::endl;
+    }
 };
 
-template<typename T, size_t S>
-struct print<T, S, false, true, true, true> // valid tuple types
+template<typename T, size_t len>
+struct print<T, len, false, true, true> // valid tuple types
 {
     const static int method = 3;
+    std::enable_if_t <myFunc::is_one_of<
+            typename std::tuple_element<0,T>::type,
+            typename std::tuple_element<len-1,T>::type,
+            len-1,
+            T>::value, void> operator()(T t)
+    {
+        std::cout << "Printing valid tuple" << std::endl;
+    }
 };
 
 template <typename T, size_t len>
-void do_print()
+void do_print(T t)
 {
-    bool is_integral = std::is_integral<T>::value;
-    bool is_object = std::is_object<T>::value;
-    bool is_tuple = myFunc::is_tuple<T>();
+    static const bool is_integral = std::is_integral<T>::value;
+    static const bool is_compound = std::is_compound<T>::value;
+    static const bool is_tuple = myFunc::is_tuple<T>();
+
+    /*
     bool is_valid_tuple = myFunc::is_one_of<
             typename std::tuple_element<0,T>::type,
             typename std::tuple_element<len,T>::type,
             len,
             T>::value;
+    */
 
-    std::cout << is_tuple << std::endl;
-    std::cout << is_valid_tuple << std::endl;
+    print<T, len, is_integral, is_compound, is_tuple>{}(t);
 }
 
 int main()
 {
     auto t1 = std::make_tuple(1,2,3,4);
-    auto t2 = std::make_tuple('1',"hello","hello","hello");
+    auto t2 = std::make_tuple('1',4.5,"he43lo","hl23lo");
     std::vector<int> t = {1,2,3,4};
 
-    do_print<decltype(t2),3>();
+    int a = 12345;
+    std::cout << sizeof(decltype(t2)) << std::endl;
+    do_print<decltype(t1),sizeof(t1)/ sizeof(int)>(t1);
 
-    //todo вызывать валидацию для тьюплов только для тьюплов
+
 
 }
