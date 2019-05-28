cb10a9159187fc09fa6f2fcd776ace5e978bf13b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 10:52:22 +0000

    Deploy alexhamster/homework_4 to github.com/alexhamster/homework_4.git:gh-pages

--- search/classes_1.js
+++ search/classes_1.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['is_5fone_5fof',['is_one_of',['../structmyFunc_1_1is__one__of.html',1,'myFunc']]],
-  ['is_5fone_5fof_3c_20std_3a_3atuple_5felement_5ft_3c_200_2c_20v_20_3e_2c_20std_3a_3atuple_5felement_5ft_3c_20i_2c_20v_20_3e_2c_20i_2d1_2c_20v_20_3e',['is_one_of&lt; std::tuple_element_t&lt; 0, V &gt;, std::tuple_element_t&lt; i, V &gt;, i-1, V &gt;',['../structmyFunc_1_1is__one__of.html',1,'myFunc']]],
-  ['is_5fone_5fof_3c_20t_2c_20t_2c_201_2c_20v_20_3e',['is_one_of&lt; T, T, 1, V &gt;',['../structmyFunc_1_1is__one__of_3_01T_00_01T_00_011_00_01V_01_4.html',1,'myFunc']]],
-  ['is_5fone_5fof_3c_20t_2c_20t_2c_20i_2c_20v_20_3e',['is_one_of&lt; T, T, i, V &gt;',['../structmyFunc_1_1is__one__of_3_01T_00_01T_00_01i_00_01V_01_4.html',1,'myFunc']]],
+  ['is_5fsame_5ftypes',['is_same_types',['../structmyFunc_1_1is__same__types.html',1,'myFunc']]],
+  ['is_5fsame_5ftypes_3c_20std_3a_3atuple_5felement_5ft_3c_200_2c_20v_20_3e_2c_20std_3a_3atuple_5felement_5ft_3c_20i_2c_20v_20_3e_2c_20i_2d1_2c_20v_20_3e',['is_same_types&lt; std::tuple_element_t&lt; 0, V &gt;, std::tuple_element_t&lt; i, V &gt;, i-1, V &gt;',['../structmyFunc_1_1is__same__types.html',1,'myFunc']]],
+  ['is_5fsame_5ftypes_3c_20t_2c_20t_2c_201_2c_20v_20_3e',['is_same_types&lt; T, T, 1, V &gt;',['../structmyFunc_1_1is__same__types_3_01T_00_01T_00_011_00_01V_01_4.html',1,'myFunc']]],
+  ['is_5fsame_5ftypes_3c_20t_2c_20t_2c_20i_2c_20v_20_3e',['is_same_types&lt; T, T, i, V &gt;',['../structmyFunc_1_1is__same__types_3_01T_00_01T_00_01i_00_01V_01_4.html',1,'myFunc']]],
   ['istupleimpl',['IsTupleImpl',['../structmyFunc_1_1IsTupleImpl.html',1,'myFunc']]],
   ['istupleimpl_3c_20std_3a_3atuple_3c_20u_2e_2e_2e_20_3e_20_3e',['IsTupleImpl&lt; std::tuple&lt; U... &gt; &gt;',['../structmyFunc_1_1IsTupleImpl_3_01std_1_1tuple_3_01U_8_8_8_01_4_01_4.html',1,'myFunc']]]
 ];
--- search/variables_2.js
+++ search/variables_2.js
@@ -3,6 +3,6 @@ var searchData=
   ['i',['i',['../unionbyteview.html#a34c79afd8afd71191848bd8fa06fd286',1,'byteview']]],
   ['is_5fcompound_5fv',['is_compound_v',['../namespacestd.html#ae1e905cdb2694181466fae8f8dd8f8e8',1,'std']]],
   ['is_5fintegral_5fv',['is_integral_v',['../namespacestd.html#a824c5eb1a7e8aafa382dc9af3329a9e8',1,'std']]],
-  ['is_5fone_5fof_5fv',['is_one_of_v',['../namespacemyFunc.html#aa757a804546f113b7cadf75b0a1ec7e1',1,'myFunc']]],
+  ['is_5fsame_5ftypes_5fv',['is_same_types_v',['../namespacemyFunc.html#a71e3910534c510ea0f7c99cc68edaf38',1,'myFunc']]],
   ['is_5fvalid_5ftuple',['is_valid_tuple',['../structprint_3_01T_00_01false_00_01true_00_01true_01_4.html#ac26cb1e1f2d4e622fc9bd7ba97555dc4',1,'print&lt; T, false, true, true &gt;']]]
 ];
