b73522e7e08b2a9db6cc16139880e982f30fa954
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-28 09:08:36 +0000

    Deploy alexhamster/homework_4 to github.com/alexhamster/homework_4.git:gh-pages

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/travis/build/alexhamster/homework_4
-# Build directory: /home/travis/build/alexhamster/homework_4
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(ipt "test_ip_print")
